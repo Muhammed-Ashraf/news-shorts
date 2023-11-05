11bb1be16108c558da812dfcc134a4ac128c9621
ashraf probook <ashraf@itware.ae>
2023-11-05 16:59:03 +0400

    Add Dependencies and versions to root buildSrc folder

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,33 @@
+object Dependencies {
+//    implementation("androidx.core:core-ktx:1.9.0")
+//    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+//    implementation("androidx.activity:activity-compose:1.8.0")
+//    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+//    implementation("androidx.compose.ui:ui")
+//    implementation("androidx.compose.ui:ui-graphics")
+//    implementation("androidx.compose.ui:ui-tooling-preview")
+//    implementation("androidx.compose.material3:material3")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+//    debugImplementation("androidx.compose.ui:ui-tooling")
+//    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
+    val lifecycleRuntimeKtx by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifeCycleRuntimeKtx}" }
+    val activityCompose by lazy { "androidx.activity:activity-compose:${Versions.activityCompose}" }
+    val composeBom by lazy { "androidx.compose:compose-bom:${Versions.composeBom}" }
+    val composeUi by lazy { "androidx.compose.ui:ui" }
+    val composeUiGraphics by lazy { "androidx.compose.ui:ui-graphics" }
+    val composeToolingPreview by lazy { "androidx.compose.ui:ui-tooling-preview" }
+    val material3 by lazy { "androidx.compose.material3:material3" }
+
+    val junit by lazy { "junit:junit:${Versions.junit}" }
+    val junitInstrumentationTest by lazy { "androidx.test.ext:junit:${Versions.junitInstrumentTest}" }
+    val espresso by lazy { "androidx.test.espresso:espresso-core:${Versions.espresso}" }
+    val composeUiTestJunit4 by lazy { "androidx.compose.ui:ui-test-junit4" }
+    val composeUiTooling by lazy { "androidx.compose.ui:ui-tooling" }
+    val composeUiTestManifest by lazy { "androidx.compose.ui:ui-test-manifest" }
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,26 @@
+object Versions {
+    //    implementation("androidx.core:core-ktx:1.9.0")
+//    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+//    implementation("androidx.activity:activity-compose:1.8.0")
+//    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+//    implementation("androidx.compose.ui:ui")
+//    implementation("androidx.compose.ui:ui-graphics")
+//    implementation("androidx.compose.ui:ui-tooling-preview")
+//    implementation("androidx.compose.material3:material3")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+//    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+//    debugImplementation("androidx.compose.ui:ui-tooling")
+//    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    const val activityCompose = "1.8.0"
+    const val composeBom = "2023.03.00"
+    const val coreKtx = "1.9.0"
+    const val lifeCycleRuntimeKtx = "2.6.2"
+
+    const val junit = "4.13.2"
+    const val junitInstrumentTest = "1.1.5"
+    const val espresso = "3.5.1"
+}
